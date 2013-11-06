32442eff94f8e9e1aaaeb77cf0156dda079f00aa
HemangPatel1 <hemang2@gmail.com>
2013-11-06 19:36:48 +0000

    saved tags are shown

--- features/step_definitions/links_steps.rb
+++ features/step_definitions/links_steps.rb
@@ -2,6 +2,7 @@ Given(/^I have saved the following links:$/) do |links|
 	links.hashes.each do |link|
     fill_in(:title, :with => link['title'])
     fill_in(:url, :with => link['url'])
+		fill_in(:tags, :with => link['tags'])
 		click_button 'Add Link'
   end
 end
--- spec/link_spec.rb
+++ spec/link_spec.rb
@@ -7,10 +7,12 @@ describe Link do
       expect(Link.count).to eq(0)
       Link.create(:title => "Makers Academy", 
                   :url => "http://www.makersacademy.com/")
+								#	:tag => ["education", "ruby"])
       expect(Link.count).to eq(1)
       link = Link.first
       expect(link.url).to eq("http://www.makersacademy.com/")
       expect(link.title).to eq("Makers Academy")
+		#	expect(link.tag).to eq(["education", "ruby"])
       link.destroy
       expect(Link.count).to eq(0)
     end
