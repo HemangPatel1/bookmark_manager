633e56276b6285ed4e238988ad2d665a5be2c035
HemangPatel1 <hemang2@gmail.com>
2013-11-06 12:05:01 +0000

    added tags corectly

--- spec/features/adding_links_spec.rb
+++ spec/features/adding_links_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 
+
 feature "User adds a new link" do
 	scenario "when browsing the homepage" do
 		expect(Link.count).to eq(0)
@@ -11,12 +12,24 @@ feature "User adds a new link" do
 		expect(link.title).to eq("Makers Academy")
 	end
 
-	def add_link(url, title)
+
+	def add_link(url, title, tags = [])
 		within('#new-link') do
 			fill_in 'url', :with => url
 			fill_in 'title', :with => title
+			fill_in 'tags', :with => tags.join(' ')
 			click_button 'Add Link'
 		end
+	end
+
+	scenario "with a few tags" do
+		visit '/'
+		add_link("http://www.makersacademy.com/",
+						 "Makers Academy",
+						 ['education', 'ruby'])
+		link = Link.first
+		expect(link.tags.map(&:text)).to include("education")
+		expect(link.tags.map(&:text)).to include("ruby")
 	end
 
 end
